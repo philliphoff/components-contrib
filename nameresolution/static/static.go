fdc85baae2b8b306ad0bf9984ac0ade16ef2068c
Phillip Hoff <phillip@orst.edu>
2021-01-29 15:21:53 -0800

    Sketch reading location from file.

--- nameresolution/static/static.go
+++ nameresolution/static/static.go
@@ -6,7 +6,10 @@
 package static
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/dapr/components-contrib/nameresolution"
 	"github.com/dapr/dapr/pkg/logger"
@@ -14,6 +17,10 @@ import (
 
 type resolver struct {
 	logger logger.Logger
+}
+
+type StaticEntry struct {
+	Host string `json:"host"`
 }
 
 // NewResolver creates static name resolver.
@@ -28,7 +35,36 @@ func (k *resolver) Init(metadata nameresolution.Metadata) error {
 
 // ResolveID resolves name to static address.
 func (k *resolver) ResolveID(req nameresolution.ResolveRequest) (string, error) {
-	var address = fmt.Sprintf("localhost:%d", req.Port)
+	var config, err = k.readConfigFileForApp(req.ID)
+	if (err != nil) {
+		return "", err
+	}
+
+	var address = fmt.Sprintf("%s:%d", config.Host, req.Port)
 	k.logger.Infof("Resolved address for ID %s to %s", req.ID, address)
 	return address, nil
+}
+
+func (k *resolver) readConfigFileForApp(id string) (*StaticEntry, error) {
+	var fileName = fmt.Sprintf("%s.json", id)
+
+	jsonFile, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	var jsonConfig StaticEntry
+	err = json.Unmarshal(byteValue, &jsonConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &jsonConfig, nil
 }
